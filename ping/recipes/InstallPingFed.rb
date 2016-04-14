98a66be2e7d2d8f7693a9044001780443678746a
psmit879 <smith.parker@sccompanies.com>
2016-04-14 18:39:32 -0500

    Update InstallPingFed.rb

--- ping/recipes/InstallPingFed.rb
+++ ping/recipes/InstallPingFed.rb
@@ -5,14 +5,14 @@ package 'java-1.8.0-openjdk'
 package 'unzip'
 
 
-directory '/var/pingtemp' do
+directory '/var/ping' do
   owner 'ec2-user'
   group 'ec2-user'
   mode '0755'
   action :create
 end
 
-remote_file '/var/pingtemp/pingfederate-8.1.2.zip' do
+remote_file '/var/ping/pingfederate-8.1.2.zip' do
   source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-8.1.2.zip'
   owner 'ec2-user'
   group 'ec2-user'
@@ -22,5 +22,10 @@ end
 
 execute 'extract_ping' do
   command 'unzip pingfederate-8.1.2.zip -d /var'
-  cwd '/var/pingtemp'
+  cwd '/var/ping'
+end
+
+execute 'move_ping' do
+  command 'mv /var/ping/pingfederate-8.1.2/pingfederate ..'
+  cwd '/var/ping'
 end

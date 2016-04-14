fcb20e83bbf3963140dbf716e35674b86ae252fe
psmit879 <smith.parker@sccompanies.com>
2016-04-14 18:07:16 -0500

    Update InstallPingFed.rb

--- ping/recipes/InstallPingFed.rb
+++ ping/recipes/InstallPingFed.rb
@@ -4,6 +4,13 @@ package 'java-1.8.0-openjdk'
 
 package 'unzip'
 
+directory '/var/pingfederate' do
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0755'
+  action :create
+end
+
 remote_file '/var/pingfederate/pingfederate-8.1.2.zip' do
   source 'https://www.pingidentity.com/content/dam/pic/downloads/software/servers/pingfederate-8.1.2.zip'
   owner 'ec2-user'
@@ -16,4 +23,3 @@ execute 'extract_ping' do
   command 'unzip pingfederate-8.1.2.zip'
   cwd '/var/pingfederate'
 end
-

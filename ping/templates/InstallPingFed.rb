95cf57ce47e27d7a36f6203b49ec73b58f1250c9
psmit879 <smith.parker@sccompanies.com>
2016-04-14 17:10:28 -0500

    Added files via upload

---
+++ ping/templates/InstallPingFed.rb
@@ -0,0 +1,17 @@
+package 'java'
+
+package 'unzip'
+
+remote_file '/var/pingfederate/pingfederate-8.1.2.zip' do
+  source 'https://www.pingidentity.com/content/dam/pic/downloads/software/servers/pingfederate-8.1.2.zip'
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0755'
+  action :create
+end
+
+execute 'extract_ping' do
+  command 'unzip pingfederate-8.1.2.zip'
+  cwd '/var/pingfederate'
+end
+

39fae17fa7cf58ec5224323eedb56805bdbfd1e7
psmit879 <smith.parker@sccompanies.com>
2016-04-15 10:08:00 -0500

    Added files via upload

--- ping/metadata.rb
+++ ping/metadata.rb
@@ -1,1 +1,8 @@
+name              'ping'
+maintainer        'Parker Smith'
+maintainer_email  'smith.parker@sccompanies.com'
+license           'Corp'
+description       'Instals PING'
+version           '0.0.1'
+
 depends 's3_file'

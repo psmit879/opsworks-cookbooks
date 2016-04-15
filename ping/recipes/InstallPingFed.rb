a868c0f4fb779e2f306020d6a1defee6403a7e25
psmit879 <smith.parker@sccompanies.com>
2016-04-14 21:09:15 -0500

    Update InstallPingFed.rb

--- ping/recipes/InstallPingFed.rb
+++ ping/recipes/InstallPingFed.rb
@@ -93,63 +93,29 @@ file '/etc/rc.d/init.d/pingfed' do
    EOH
   action :create
 end
-
-#remote_file '/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip' do
-#  source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-data-04-14-2016.zip'
-#  owner 'pingfed'
-#  group 'pingfed'
-#  mode '0775'
-#  action :create
-#  not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
-#end
-#
-#remote_file '/pingfederate/server/default/conf/pingfederate.lic' do
-#  source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/PingFederate.78200.Development.lic'
-#  owner 'pingfed'
-#  group 'pingfed'
-#  mode '0775'
-#  action :create
-#  not_if { ::File.exists?('/pingfederate/server/default/conf/pingfederate.lic') }
-#end
-
-require 'aws-sdk'
-
-s3 = AWS::S3.new
-
-# Set bucket and object name
-obj = s3.buckets['colonysecurity-apps'].objects['PINGFed/pingfederate-data-04-14-2016.zip']
-
-# Read content to variable
-file_content = obj.read
-
-# Log output (optional)
-Chef::Log.info(file_content)
-
-# Write content to file
-file '/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip' do
+#https://github.com/adamsb6/s3_file
+s3_file '/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip' do
+  #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-data-04-14-2016.zip'
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/pingfederate-data-04-14-2016.zip"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'
-  content file_content
   action :create
+  not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
 end
 
-obj = s3.buckets['colonysecurity-apps'].objects['PINGFed/PingFederate.78200.Development.lic']
-
-# Read content to variable
-file_content = obj.read
-
-# Log output (optional)
-Chef::Log.info(file_content)
-
-# Write content to file
-file '/pingfederate/server/default/conf/pingfederate.lic' do
+s3_file '/pingfederate/server/default/conf/pingfederate.lic' do
+  #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/PingFederate.78200.Development.lic'
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/PingFederate.78200.Development.lic"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'
-  content file_content
   action :create
+  not_if { ::File.exists?('/pingfederate/server/default/conf/pingfederate.lic') }
 end
+
 
 script 'start_service' do
   interpreter "bash"

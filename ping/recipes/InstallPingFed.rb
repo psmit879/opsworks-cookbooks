956838e2150783d561bc46c43bf704e9f3b556dc
psmit879 <smith.parker@sccompanies.com>
2016-04-14 19:00:35 -0500

    Update InstallPingFed.rb

--- ping/recipes/InstallPingFed.rb
+++ ping/recipes/InstallPingFed.rb
@@ -4,19 +4,24 @@ package 'java-1.8.0-openjdk'
 
 package 'unzip'
 
+user 'pingfed' do
+  comment 'pingfed'
+  system true
+  shell '/bin/bash'
+end
 
 directory '/var/ping' do
-  owner 'ec2-user'
-  group 'ec2-user'
-  mode '0755'
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
   action :create
 end
 
 remote_file '/var/ping/pingfederate-8.1.2.zip' do
   source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-8.1.2.zip'
-  owner 'ec2-user'
-  group 'ec2-user'
-  mode '0755'
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
   action :create
 end
 
@@ -28,4 +33,71 @@ end
 execute 'move_ping' do
   command 'mv /var/ping/pingfederate-8.1.2/pingfederate ..'
   cwd '/var/ping'
+end
+
+file '/etc/rc.d/init.d/pingfed' do
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0755'
+  content <<-EOH
+	#! /bin/sh
+
+   start(){
+      echo "starting PingFederate.."
+      su - pingfed \
+      -c '/var/ping/pingfederate/sbin/pingfederate-run.sh \
+      > /dev/null 2> /dev/null'
+   }
+   stop(){
+      echo "stopping PingFederate.."
+      su - pingfed \
+      -c '/var/ping/pingfederate/sbin/\
+         pingfederate-shutdown.sh'
+   }
+
+   restart(){
+      stop
+   # padding time to stop before restart
+      sleep 60
+   # To protect against any services that are not stopped,
+   # uncomment the following command.
+   # (Warning: this kills all Java instances running as
+   # pingfed.)
+   #   su - pingfed -c 'killall java'
+      start
+   }
+   case "$1" in
+      start)
+         start
+         ;;
+      stop)
+         stop
+         ;;
+      restart)
+         restart
+         ;;
+      *)
+      echo "Usage: pingfed {start|stop|restart}"
+      exit 1
+   esac
+   exit 0
+   EOH
+  action :create
+end
+
+
+script 'start_service' do
+  interpreter "bash"
+  cwd '/home/ec2-user'
+  code <<-EOH
+  	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc3.d/S84pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc5.d/S84pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc4.d/S84pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc6.d/K15pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc0.d/K15pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc1.d/K15pingfed
+	ln -s /etc/rc.d/init.d/<pf_user> /etc/rc2.d/K15pingfed
+	chmod 755 /etc/rc.d/init.d/pingfed
+	service pingfed start
+  	EOH
 end

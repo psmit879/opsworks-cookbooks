d069c1b3f3a3e3005879902293543a20e2945b15
psmit879 <smith.parker@sccompanies.com>
2016-04-18 15:32:35 -0500

    Update ConfigureEngine.rb

--- ping/recipes/ConfigureEngine.rb
+++ ping/recipes/ConfigureEngine.rb
@@ -1,8 +1,8 @@
-execute 'rm -f /var/ping/pingfederate/server/default/conf/tcp.xml'
+execute 'rm -f /var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml'
 
-execute 'rm -f /var/ping/pingfederate/bin/run.properties'
+execute 'rm -f /var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties'
 
-s3_file '/var/ping/pingfederate/server/default/conf/tcp.xml' do
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/tcp.xml'
   bucket "colonysecurity-apps"
   remote_path "/PINGFed/tcp.xml"
@@ -13,7 +13,7 @@ s3_file '/var/ping/pingfederate/server/default/conf/tcp.xml' do
   #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
 end
 
-s3_file '/var/ping/pingfederate/bin/run.properties' do
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/run.properties'
   bucket "colonysecurity-apps"
   remote_path "/PINGFed/run.properties"

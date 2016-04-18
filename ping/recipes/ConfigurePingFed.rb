f676ab3d4944dc9860dc4fbf0ddf0f51c833bcad
psmit879 <smith.parker@sccompanies.com>
2016-04-18 13:24:19 -0500

    Create ConfigurePingFed.rb

---
+++ ping/recipes/ConfigurePingFed.rb
@@ -0,0 +1,22 @@
+#https://github.com/adamsb6/s3_file
+s3_file '/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip' do
+  #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-data-04-14-2016.zip'
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/pingfederate-data-04-14-2016.zip"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+end
+
+s3_file '/var/ping/pingfederate/server/default/conf/pingfederate.lic' do
+  #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/PingFederate.78200.Development.lic'
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/PingFederate.78200.Development.lic"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('/var/ping/pingfederate/server/default/conf/pingfederate.lic') }
+end

d9894681f040d542f9a9231881e76f91974d70ae
psmit879 <smith.parker@sccompanies.com>
2016-04-20 10:10:47 -0500

    Update ConfigureConsole.rb

--- ping/recipes/ConfigureConsole.rb
+++ ping/recipes/ConfigureConsole.rb
@@ -10,6 +10,8 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/data/drop-in-d
   not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
 end
 
+#sed -i 's/ADLICENSE="Y"/ADLICENSE="N"/g' centrifydc-install.cfg
+
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/pingfederate.lic' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/PingFederate.78200.Development.lic'
   bucket "colonysecurity-apps"

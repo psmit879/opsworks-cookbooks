c0aa02f90261d201b05525290911312d031c869e
psmit879 <smith.parker@sccompanies.com>
2016-04-20 10:17:02 -0500

    Update ConfigureEngine.rb

--- ping/recipes/ConfigureEngine.rb
+++ ping/recipes/ConfigureEngine.rb
@@ -13,6 +13,36 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' 
   #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
 end
 
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/pf-pingid-idp-adapter-1.3.1.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/pf-pingid-idp-adapter-1.3.1.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+end
+
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/gson-2.2.4.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/gson-2.2.4.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+end
+
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/common-mfa-14.4.7.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/common-mfa-14.4.7.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+end
+
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/run.properties'
   bucket "colonysecurity-apps"

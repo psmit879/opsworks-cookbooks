2a2b471808c6d43a657a7fec8db72871cff79d61
psmit879 <smith.parker@sccompanies.com>
2016-04-22 15:13:35 -0500

    Added files via upload

---
+++ vshell/metadata.rb
@@ -0,0 +1,9 @@
+name              'vshell'
+maintainer        'Parker Smith'
+maintainer_email  'smith.parker@sccompanies.com'
+license           'Corp'
+description       'Instals vShell'
+version           '0.0.1'
+
+depends 's3_file'
+depends 'windows'
---
+++ vshell/recipes/install.rb
@@ -0,0 +1,33 @@
+directory 'C:\tmp' do
+  inherits true
+  action :create
+  not_if { ::File.exists?('C:\tmp') }
+end
+
+directory 'C:\tmp\vShell' do
+  inherits true
+  action :create
+  not_if { ::File.exists?('C:\tmp\vShell') }
+end
+
+s3_file 'C:\tmp\vShell\vShellInstall.zip' do
+  bucket "colonysecurity-apps"
+  remote_path "/vShell/vShellInstall.zip"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('C:\tmp\vShell\vShellInstall.zip') }
+end
+
+windows_zipfile 'C:\tmp\vShell' do
+  source 'C:\tmp\vShell\vShellInstall.zip'
+  action :unzip
+  not_if { ::File.exists?('C:\tmp\vShell\vShellInstall') }
+end
+
+windows_package 'vShell' do
+  action :install
+  option '/s'
+  source 'C:\tmp\vShell\vShellInstall\vshell-ftps-x64.4.2.0.980.exe'
+end

67b4fd2fb104c70db0d272b773346e579a5c0583
psmit879 <smith.parker@sccompanies.com>
2016-04-22 15:59:33 -0500

    Update ConfigureConsole.rb

--- ping/recipes/ConfigureConsole.rb
+++ ping/recipes/ConfigureConsole.rb
@@ -1,7 +1,18 @@
 #https://github.com/adamsb6/s3_file
-execute 'rm -f /var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml'
+file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
+  action :delete
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
 
-execute 'rm -f /var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties'
+file '/var/ping/pingfederate-8.1.2/pingfederate/bin/ldap.properties' do
+  action :delete
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
+
+file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' do
+  action :delete
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/tcp.xml'
@@ -11,7 +22,37 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' 
   group 'pingfed'
   mode '0775'
   action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
+
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/pf-pingid-idp-adapter-1.3.1.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/pf-pingid-idp-adapter-1.3.1.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
+
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/gson-2.2.4.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/gson-2.2.4.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
+
+s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/common-mfa-14.4.7.jar' do
+  bucket "colonysecurity-apps"
+  remote_path "/PINGFed/common-mfa-14.4.7.jar"
+  owner 'pingfed'
+  group 'pingfed'
+  mode '0775'
+  action :create
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/bin/ldap.properties' do
@@ -22,7 +63,7 @@ s3_file '/var/ping/pingfederate-8.1.2/bin/ldap.properties' do
   group 'pingfed'
   mode '0775'
   action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/data/drop-in-deployer/data.zip' do
@@ -33,7 +74,7 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/data/drop-in-d
   group 'pingfed'
   mode '0775'
   action :create
-  not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 #sed -i 's/ADLICENSE="Y"/ADLICENSE="N"/g' centrifydc-install.cfg
@@ -46,37 +87,7 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/pingfeder
   group 'pingfed'
   mode '0775'
   action :create
-  not_if { ::File.exists?('/var/ping/pingfederate/server/default/conf/pingfederate.lic') }
-end
-
-s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/pf-pingid-idp-adapter-1.3.1.jar' do
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/pf-pingid-idp-adapter-1.3.1.jar"
-  owner 'pingfed'
-  group 'pingfed'
-  mode '0775'
-  action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
-end
-
-s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/gson-2.2.4.jar' do
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/gson-2.2.4.jar"
-  owner 'pingfed'
-  group 'pingfed'
-  mode '0775'
-  action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
-end
-
-s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/common-mfa-14.4.7.jar' do
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/common-mfa-14.4.7.jar"
-  owner 'pingfed'
-  group 'pingfed'
-  mode '0775'
-  action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/data/drop-in-deployer/data.zip') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
@@ -87,9 +98,20 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
   group 'pingfed'
   mode '0775'
   action :create
-  #not_if { ::File.exists?('/var/ping/pingfederate/server/default/conf/pingfederate.lic') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 execute 'chown -R pingfed /var/ping'
 
-execute 'service pingfed start'
+file '/var/ping/configureComplete' do
+  content 'This is a flag that prevents the configure recipe from running a 2nd time'
+  mode '0775'
+  owner 'pingfed'
+  group 'pingfed'
+  not_if { ::File.exists?('/var/ping/configureComplete') }
+end
+
+#execute 'service pingfed start'
+service "pingfed" do
+  action :start
+end

e4a052883e9011599e1421c5eca222ffe7d94292
psmit879 <smith.parker@sccompanies.com>
2016-05-16 17:20:57 -0500

    Update ConfigureProdConsole.rb

--- ping/recipes/ConfigureProdConsole.rb
+++ ping/recipes/ConfigureProdConsole.rb
@@ -16,8 +16,8 @@ end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/tcp.xml'
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/tcp.xml"
+  bucket "colonybrands-security-apps"
+  remote_path "/PINGFed/tcp.xml-PROD"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'
@@ -26,7 +26,7 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/tcp.xml' 
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/pf-pingid-idp-adapter-1.3.1.jar' do
-  bucket "colonysecurity-apps"
+  bucket "colonybrands-security-apps"
   remote_path "/PINGFed/pf-pingid-idp-adapter-1.3.1.jar"
   owner 'pingfed'
   group 'pingfed'
@@ -36,7 +36,7 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/pf-ping
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/gson-2.2.4.jar' do
-  bucket "colonysecurity-apps"
+  bucket "colonybrands-security-apps"
   remote_path "/PINGFed/gson-2.2.4.jar"
   owner 'pingfed'
   group 'pingfed'
@@ -46,7 +46,7 @@ s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/gson-2.
 end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/deploy/common-mfa-14.4.7.jar' do
-  bucket "colonysecurity-apps"
+  bucket "colonybrands-security-apps"
   remote_path "/PINGFed/common-mfa-14.4.7.jar"
   owner 'pingfed'
   group 'pingfed'
@@ -57,7 +57,7 @@ end
 
 s3_file '/var/ping/pingfederate-8.1.2/bin/ldap.properties' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/tcp.xml'
-  bucket "colonysecurity-apps"
+  bucket "colonybrands-security-apps"
   remote_path "/PINGFed/ldap.properties"
   owner 'pingfed'
   group 'pingfed'
@@ -68,7 +68,7 @@ end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/data/drop-in-deployer/data.zip' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/pingfederate-data-04-14-2016.zip'
-  bucket "colonysecurity-apps"
+  bucket "colonybrands-security-apps"
   remote_path "/PINGFed/pingfederate-data-04-20-2016.zip"
   owner 'pingfed'
   group 'pingfed'
@@ -81,8 +81,8 @@ end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/server/default/conf/pingfederate.lic' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/PingFederate.78200.Development.lic'
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/PingFederate.78200.Development.lic"
+  bucket "colonybrands-security-apps"
+  remote_path "/PINGFed/PingFederate.78199.Production.lic"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'
@@ -92,8 +92,8 @@ end
 
 s3_file '/var/ping/pingfederate-8.1.2/pingfederate/bin/run.properties' do
   #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/run.properties'
-  bucket "colonysecurity-apps"
-  remote_path "/PINGFed/run.properties-CONSOLE"
+  bucket "colonybrands-security-apps"
+  remote_path "/PINGFed/run.properties-PRODCONSOLE"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'

38d3a060cd347e8f53b9499ba48c8f76b863edce
psmit879 <smith.parker@sccompanies.com>
2016-05-16 20:32:27 -0500

    Update InstallPingProd.rb

--- ping/recipes/InstallPingProd.rb
+++ ping/recipes/InstallPingProd.rb
@@ -30,13 +30,24 @@ directory '/home/pingfed' do
   not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
-remote_file '/var/ping/pingfederate-8.1.2.zip' do
-  source 'https://s3.amazonaws.com/colonybrands-security-apps/PINGFed/pingfederate-8.1.2.zip'
+#remote_file '/var/ping/pingfederate-8.1.2.zip' do
+#  source 'https://s3.amazonaws.com/colonybrands-security-apps/PINGFed/pingfederate-8.1.2.zip'
+#  owner 'pingfed'
+#  group 'pingfed'
+#  mode '0775'
+#  action :create
+#  not_if { ::File.exists?('/var/ping/pingfederate-8.1.2.zip') }
+#end
+
+s3_file '/var/ping/pingfederate-8.1.2.zip' do
+  #source 'https://s3.amazonaws.com/colonysecurity-apps/PINGFed/tcp.xml'
+  bucket "colonybrands-security-apps"
+  remote_path "/PINGFed/pingfederate-8.1.2.zip"
   owner 'pingfed'
   group 'pingfed'
   mode '0775'
   action :create
-  not_if { ::File.exists?('/var/ping/pingfederate-8.1.2.zip') }
+  not_if { ::File.exists?('/var/ping/configureComplete') }
 end
 
 execute 'extract_ping' do

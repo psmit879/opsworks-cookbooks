1225e2501f6383e542030d6c0871ae1db4848993
psmit879 <smith.parker@sccompanies.com>
2016-05-31 17:20:20 -0500

    Update configcerts.rb

--- security/recipes/configcerts.rb
+++ security/recipes/configcerts.rb
@@ -1,10 +1,10 @@
 powershell_script 'configureGateway' do
   code <<-EOH
-	New-SelfSignedCertificate -DnsName "RDGateway.sccompanies.com" -CertStoreLocation "Cert:\LocalMachine\My"
+	New-SelfSignedCertificate -DnsName "RDGateway.sccompanies.com" -CertStoreLocation "Cert:\\LocalMachine\\My"
 	Import-Module remotedesktopservices
-	new-item -path RDS:\GatewayServer\CAP -Name Default-CAP -UserGroups 'Domain Users@Train' -AuthMethod 1
-	new-item -Path RDS:\GatewayServer\RAP -Name Default-RAP -UserGroups 'Domain Users@Train' -ComputerGroupType 1 -ComputerGroup 'Domain Computers@Train’
-	dir cert:\localmachine\my | where-object { $_.Subject -eq "CN=rdgateway.sccompanies.com” } | ForEach-Object { Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $_.Thumbprint }
+	new-item -path RDS:\\GatewayServer\\CAP -Name Default-CAP -UserGroups 'Domain Users@Train' -AuthMethod 1
+	new-item -Path RDS:\\GatewayServer\\RAP -Name Default-RAP -UserGroups 'Domain Users@Train' -ComputerGroupType 1 -ComputerGroup 'Domain Computers@Train’
+	dir cert:\\localmachine\\my | where-object { $_.Subject -eq "CN=rdgateway.sccompanies.com” } | ForEach-Object { Set-Item -Path RDS:\\GatewayServer\\SSLCertificate\\Thumbprint -Value $_.Thumbprint }
 	Restart-Service TSGateway
 
 EOH

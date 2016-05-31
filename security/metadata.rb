6badbf36af673ae10aa1b5599f09f19a1f6dfdf3
psmit879 <smith.parker@sccompanies.com>
2016-05-31 14:56:04 -0500

    Add files via upload

---
+++ security/metadata.rb
@@ -0,0 +1,9 @@
+name              'security'
+maintainer        'Parker Smith'
+maintainer_email  'smith.parker@sccompanies.com'
+license           'Corp'
+description       'Instals security'
+version           '0.0.1'
+
+depends 's3_file'
+depends 'windows'
---
+++ security/recipes/windowsjump.rb
@@ -0,0 +1,7 @@
+windows_feature 'Gateway' do
+  action :install
+end
+
+windows_feature 'Gateway-UI' do
+  action :install
+end
